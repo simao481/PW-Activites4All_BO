9eb0d581d6b885bfa9cac4047ccd81802a67fc36
borges023 <luanaborges2002@gmail.com>
2023-06-02 16:45:51 +0100

    certo

--- vue-paper-dashboard-master/src/router/routes.js
+++ vue-paper-dashboard-master/src/router/routes.js
@@ -3,13 +3,13 @@ import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
 import NotFound from "@/pages/NotFoundPage.vue";
 
 // Admin pages
-import Dashboard from "@/pages/Dashboard.vue";
+import Estatisticas from "@/pages/Estatisticas.vue";
 import UserProfile from "@/pages/UserProfile.vue";
 import Notifications from "@/pages/Notifications.vue";
-import Icons from "@/pages/Icons.vue";
-import Maps from "@/pages/Maps.vue";
-import Typography from "@/pages/Typography.vue";
-import TableList from "@/pages/TableList.vue";
+import Atividades from "@/pages/Atividades.vue";
+import Sede from "@/pages/Sede.vue";
+import Profissionais from "@/pages/Profissionais.vue";
+import Reservas from "@/pages/Reservas.vue";
 import Detalhes from "@/pages/Detalhes.vue";
 
 
@@ -21,8 +21,8 @@ const routes = [
     children: [
       {
         path: "dashboard",
-        name: "dashboard",
-        component: Dashboard,
+        name: "Estatisticas",
+        component: Estatisticas,
       },
       {
         path: "stats",
@@ -36,23 +36,23 @@ const routes = [
       },
       {
         path: "icons",
-        name: "icons",
-        component: Icons,
+        name: "Atividades",
+        component: Atividades,
       },
       {
         path: "maps",
-        name: "maps",
-        component: Maps,
+        name: "Sede",
+        component: Sede,
       },
       {
         path: "typography",
-        name: "typography",
-        component: Typography,
+        name: "Profissionais",
+        component: Profissionais,
       },
       {
         path: "table-list",
-        name: "table-list",
-        component: TableList,
+        name: "Reservas",
+        component: Reservas,
       },
       {
         path: "detalhes/:id",
